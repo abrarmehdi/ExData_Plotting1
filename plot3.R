741127f168ab066af4b6ca8101edb893ae8e74cf
Abrar Mehdi <35421321+abrarmehdi@users.noreply.github.com>
2019-02-03 19:15:26 +0530

    Updated Comments

--- plot3.R
+++ plot3.R
@@ -1,10 +1,26 @@
+# Reading Data File
 powerData <- read.table("..//Data Set//household_power_consumption.txt", header = TRUE, sep = ";")
+
+# Extracting Two Days Data
 twoDaysData <- powerData[powerData$Date %in% c("1/2/2007","2/2/2007"),]
-twoDaysData$Global_active_power <- as.numeric(as.character(twoDaysData$Global_active_power))
+
+# Creating PNG file
 png(filename = "plot3.png", width = 480, height = 480)
+
+# Creating TimeStamp by combining Date and Time
 x <- strptime(as.character(paste(twoDaysData$Date,twoDaysData$Time, sep=" ")), "%d/%m/%Y%n%T")
-with(twoDaysData, plot(x, twoDaysData$Sub_metering_1, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+# Creating Plot
+with(twoDaysData, plot(x, twoDaysData$Sub_metering_1, type="l", xlab = "", ylab = "Energy Sub Metering"))
+
+# Adding Second Plot
 lines(x, twoDaysData$Sub_metering_2, col = "Red")
+
+# Adding Thrid Plot
 lines(x, twoDaysData$Sub_metering_3, col = "Blue")
+
+# Adding Legends
 legend("topright", 30, legend=c("Sub Metering 1", "Sub Metering 2", "Sub Metering 3"),col=c("Black", "Red", "Blue"), lty = 1, cex=0.8)
+
+# Closing Device
 dev.off()
