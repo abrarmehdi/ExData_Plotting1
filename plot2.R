e8f03cce9cd6786393a6e4971e4378fadfa97a40
Abrar Mehdi <35421321+abrarmehdi@users.noreply.github.com>
2019-02-03 19:12:49 +0530

    Updated Comments

--- plot2.R
+++ plot2.R
@@ -1,6 +1,20 @@
+# Reading Data File
 powerData <- read.table("..//Data Set//household_power_consumption.txt", header = TRUE, sep = ";")
+
+# Extracting Two Days Data
 twoDaysData <- powerData[powerData$Date %in% c("1/2/2007","2/2/2007"),]
+
+# Converting Global Active Power values to numeric
 twoDaysData$Global_active_power <- as.numeric(as.character(twoDaysData$Global_active_power))
+
+# Creating PNG file
 png(filename = "plot2.png", width = 480, height = 480)
-with(twoDaysData, plot(strptime(as.character(paste(twoDaysData$Date,twoDaysData$Time, sep=" ")), "%d/%m/%Y%n%T"), twoDaysData$Global_active_power, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+# Creating Time by combining Date and Time
+x <- strptime(as.character(paste(twoDaysData$Date,twoDaysData$Time, sep=" ")), "%d/%m/%Y%n%T")
+
+# Creating Plot
+with(twoDaysData, plot(x, twoDaysData$Global_active_power, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+# Closing Device
 dev.off()
