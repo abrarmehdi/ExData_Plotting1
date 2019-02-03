ea110452a85c0f65f3cf98e9ec4dc1f16cca1f5e
Abrar Mehdi <35421321+abrarmehdi@users.noreply.github.com>
2019-02-03 19:09:31 +0530

    Updated Comments

--- plot1.R
+++ plot1.R
@@ -1,6 +1,17 @@
+# Reading Data File
 powerData <- read.table("..//Data Set//household_power_consumption.txt", header = TRUE, sep = ";")
+
+# Extracting Two Days Data
 twoDaysData <- powerData[powerData$Date %in% c("1/2/2007","2/2/2007"),]
+
+# Converting Global Active Power values to numeric
 twoDaysData$Global_active_power <- as.numeric(as.character(twoDaysData$Global_active_power))
+
+# Creating PNG file
 png(filename = "plot1.png", width = 480, height = 480)
+
+# Creating Histogram
 with(twoDaysData, hist(twoDaysData$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col="Red", main = "Global Active Power",xlim = c(0,8), ylim = c(0,1200)))
+
+# Closing Device
 dev.off()
